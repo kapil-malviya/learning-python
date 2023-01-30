d61bfd45f7c0cad41e8f33af7ab6c349e32a6747
kapil-malviya <etc.kapil@gmail.com>
2023-01-30 16:05:41 +0530

    in points and exponential form

---
+++ float-datatype.py
@@ -0,0 +1,9 @@
+a=1.23
+b=1.2e3           # exponential form is valid in float but the binary, octal and hexa-decimal form
+c=1.2e100
+print(type(a))
+print(type(b))
+print(type(c))
+print(a)
+print(b)
+print(c)
