8d413c8775ecc7425e9f8674095e594e67656d14
kapil-malviya <etc.kapil@gmail.com>
2023-01-31 13:16:03 +0530

    string variations

---
+++ string-datatype.py
@@ -0,0 +1,28 @@
+# string = sequence of characters 
+# In Python, we can use single, double, or triple (single/double) quotes to define a string
+# but Python always follows single quote and thus output is always in single quote
+# so it is recommended or by convention to use single quote
+
+a1='kapil' 'malviya'
+a2="kapil" 'malviya'                # for multiline purpose we use triple (single/double) quotes
+a3='''ka   pil'''                       
+a4="""kapil
+      malviya"""
+a5='''kapil malviya'''
+a6="my name is 'kapil' and i'm from indore"
+
+print(a1)
+print(a2)
+print(a3)
+print(a4)
+print(a5)
+print(a6)
+print(type(a1))
+print(type(a2))
+print(type(a3))
+print(type(a4))
+print(type(a5))
+print(type(a6))
+
+
+# slice operator in next program
