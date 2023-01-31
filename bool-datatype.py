e80a24aa2d6ce63bc031b3fdb39e13e67ec7b1d9
kapil-malviya <etc.kapil@gmail.com>
2023-01-31 10:50:30 +0530

    only allowed True & False

---
+++ bool-datatype.py
@@ -0,0 +1,19 @@
+a=20
+b=30
+c=a<b       # true
+d=a>b       # false
+print(c)
+print(d)
+                           # True = 1
+                           # False = 0
+a1=True+True
+print("True + True = ", a1)    # output = 2
+
+a2=True
+a3=a1+a2
+print("True + True + True = ", a3)    # output = 3
+
+a4=False
+print("True + True + False = ", a1+a4)   # output = 2
+print("False + False = ", a4+a4)   # output = 0
+print("True + False = ", a2+a4)   # output = 1
