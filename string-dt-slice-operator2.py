9ea2e356518163c37865ae80614e13b2c817c2eb
kapil-malviya <etc.kapil@gmail.com>
2023-02-01 11:50:46 +0530

    string-datatype slice-operator jump(step)

---
+++ string-dt-slice-operator2.py
@@ -0,0 +1,24 @@
+# slice (piece) operator
+
+# even char values can be represented by the string
+
+s1="my $ name is KAPIL and i'm not an Artist"
+
+print(type(s1))
+print(s1)
+print(s1[6])
+print(s1[-4])
+print(s1[-8:-3])
+print(s1[3:18])
+# s1[begin : end : step]  step= jump by the numbers
+print(s1[2:18:2])
+print(s1[0:30:3])
+print(s1[-30:-1:2])
+
+# print multiple times
+
+print(s1*10)
+
+# len() to get length of the string 
+
+print(len(s1))
