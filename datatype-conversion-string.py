5518e527872544af5fe268a2363843850c809939
kapil-malviya <etc.kapil@gmail.com>
2023-02-01 16:06:18 +0530

    conversion of any type of data-type to string-type

---
+++ datatype-conversion-string.py
@@ -0,0 +1,27 @@
+# Type casting or Type coersion or Type conversion
+	# from one type of data type to another type of datatype
+#  int(anytype to int)  float(... to float)  complex(..to complex)  str(..to string datatype)
+
+# str()  
+  # nothing is restricted to convert value any type of value can be converted
+
+a1=88
+a2=0
+a=123.456  
+b=10+20j   
+b1=0j
+c=True     
+d=False    
+h=0b11001  
+i=0o11001  
+j=0x11001  
+print(str(a1))
+print(str(a2))
+print(str(a))
+print(str(b))
+print(str(b1))
+print(str(c))
+print(str(d))
+print(str(h))
+print(str(i))
+print(str(j))
