a8fcb0bf2e7c091fd3514f0d0d6a7870fc616fc5
kapil-malviya <etc.kapil@gmail.com>
2023-02-02 10:18:19 +0530

    arithmatic operators

---
+++ all-arithmatic-operation.py
@@ -0,0 +1,14 @@
+var=56
+varr=13
+add=var+varr
+sub=var-varr
+mul=var*varr
+div=var/varr
+mod=var%varr
+flrdiv=var//varr
+print("sum of two variable : ", add)
+print("substration is : ", sub)
+print("multiplication is :", mul)
+print('divison is :', div)
+print("remainder is :", mod)
+print("floor division is :", flrdiv)
