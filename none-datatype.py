492e47d41c9721a46c5662679a68a2e393cdad37
kapil-malviya <etc.kapil@gmail.com>
2023-02-05 16:24:04 +0530

    none type with def keyword and pass operation

---
+++ none-datatype.py
@@ -0,0 +1,27 @@
+# none datatype
+
+    # used to define a null value, or no value at all
+    # None is not the same as 0, False, or an empty string. None is a data type of its own (NoneType) and only None can be None
+
+
+def f1() :
+	a=10
+
+  # def keyword used to define methods and functions in python
+
+def f2() :
+	print("hellooo...")
+
+print(f2())
+print()
+print(f1())
+print()
+print()
+print()
+
+ # nothing, just pass...
+
+def f3():
+	pass
+print("operation passed")
+
