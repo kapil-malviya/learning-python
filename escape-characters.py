4b65beab48f69914e8000c634eeb3047d2d0b050
kapil-malviya <etc.kapil@gmail.com>
2023-02-05 17:42:25 +0530

    escape characters

---
+++ escape-characters.py
@@ -0,0 +1,22 @@
+# escape character
+   # \n, \t, \r, \b, \f, \', \\, \v
+
+
+o='kapil\nmalviya'   # change line
+p='kapil\tmalviya'   # give much space
+
+print(o)
+print(p)
+print()
+
+# \'
+
+x= 'our prime minister\'s modi is amazing person'   
+print(x)
+print()
+
+# constants
+  # which value doesn't change
+
+  # define constant in python
+    # no way to specify in python
