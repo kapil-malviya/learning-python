ce2dc3382bf6ccb35d3020cc110bc575642b7561
kapil-malviya <etc.kapil@gmail.com>
2023-02-06 10:54:54 +0530

    arithmatic operation with different types of values

---
+++ operators-arithmatic-1.py
@@ -0,0 +1,74 @@
+#  arithmatic operator
+    # with complex,binary,octal and hexa-decimal values
+
+a1=48+8j
+a2=18.5+4j
+b1=0b101101
+b2=0b100101
+c1=0o123456
+c2=0o764213
+d1=0x185816
+d2=0x84128
+e1=True
+e2=False
+
+
+print("a1+a2 =",a1+a2)
+print("a1+b1 =",a1+b1)
+print("a1+c1 =",a1+c1)
+print("a1+d1 =",a1+d1)
+print("c1+c2 =",c1+c2)
+print("c1+d2 =",c1+d2)
+print("c2+b2 =",c2+b2)
+print("e1+e2 =",e1+e2)
+print()
+
+
+print("a1-a2 =",a1-a2)
+print("a1-b1 =",a1-b1)
+print("a1-c1 =",a1-c1)
+print("a1-d1 =",a1-d1)
+print("c1-c2 =",c1-c2)
+print("c1-d2 =",c1-d2)
+print("c2-b2 =",c2-b2)
+print("e1-e2 =",e1-e2)
+print()
+
+print("a1*a2 =",a1*a2)
+print("a1*b1 =",a1*b1)
+print("a1*c1 =",a1*c1)
+print("a1*d1 =",a1*d1)
+print("c1*c2 =",c1*c2)
+print("c1*d2 =",c1*d2)
+print("c2*b2 =",c2*b2)
+print("e1*e2 =",e1*e2)
+print()
+
+print("a1/a2 =",a1/a2)
+print("a1/b1 =",a1/b1)
+print("a1/c1 =",a1/c1)
+print("a1/d1 =",a1/d1)
+print("c1/c2 =",c1/c2)
+print("c1/d2 =",c1/d2)
+print("c2/b2 =",c2/b2)
+# print("e1/e2 =",e1/e2) ZeroDivisionError
+print()
+
+# print("a1//a2 =",a1//a2)
+# print("a1//b1 =",a1//b1)
+# print("a1//c1 =",a1//c1)
+# print("a1//d1 =",a1//d1)
+print("c1//c2 =",c1//c2)
+print("c1//d2 =",c1//d2)
+print("c2//b2 =",c2//b2)
+print()
+
+# print("a1%a2 =",a1%a2)
+# print("a1%b1 =",a1%b1)
+# print("a1%c1 =",a1%c1)
+# print("a1%d1 =",a1%d1)
+print("c1%c2 =",c1%c2)
+print("c1%d2 =",c1%d2)
+print("c2%b2 =",c2%b2)
+print()
+
