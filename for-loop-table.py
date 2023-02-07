8a7795c0e53ff1abe264bbf5c95ebc2fc2ccff1b
kapil-malviya <etc.kapil@gmail.com>
2023-02-07 12:24:18 +0530

    for loop (table) and with format function in output('{}{}'.format(i,j)

---
+++ for-loop-table.py
@@ -0,0 +1,31 @@
+#for loop 
+
+
+# for i in range(2,21,2):       
+# 	print(i)              
+
+
+# user input table 
+
+# num=int(input('enter value : '))
+# for i in range (num,num*10-1,num):
+# 	print(i)
+
+
+# user input table =>
+
+# x=int(input("Enter any number : "))
+# for i in range (1,11):
+# 	z=x*i
+# 	print(x,'*',i,'=',z)  
+
+
+   # another way ( formate function )
+
+x=int(input("Enter any number : "))
+for i in range (1,11):
+	z=x*i
+	print('{}*{}={}'.format(x,i,z))
+	  # {} => won't consider in string inside curly braces
+
+
