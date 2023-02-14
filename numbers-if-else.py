dc65ec4b385d8758712339d90ca853b58d406c1f
kapil-malviya <etc.kapil@gmail.com>
2023-02-14 09:53:51 +0530

    if else statement (numbers)

---
+++ numbers-if-else.py
@@ -0,0 +1,61 @@
+### program to find biggest of 2 numbers from the user
+
+
+# n1=int(input('Enter first number : '))
+# n2=int(input('Enter second number : '))
+# if n1>n2 :
+# 	print('Bigger no. is ',n1)
+# else :
+# 	print('Bigger no. is ',n2)
+
+
+## with eval function
+
+# n1=eval(input('Enter first number : '))
+# n2=eval(input('Enter second number : '))
+# if n1>n2 :
+# 	print('Bigger no. is ',n1)
+# else :
+# 	print('Bigger no. is ',n2)
+
+
+### biggest of given 3 numbers
+
+
+# n1=eval(input('Enter first number : '))
+# n2=eval(input('Enter second number : '))
+# n3=eval(input('Enter third number : '))
+# if n1>n2 and n1>n3 :
+# 	print('Biggest no. is ',n1)
+# elif n2>n3 :
+# 	print('Biggest no. is ',n2)
+# else :
+# 	print('Biggest no. is ',n3)
+
+
+
+### print no. in between 0 and 9
+
+n=int(input('Enter a digit from 0 to 9 : '))
+if n == 0 :
+	print('Zero')
+elif n==1 :
+	print('One')
+elif n==2 :
+	print('Two')
+elif n==3 :
+	print('Three')
+elif n==4 :
+	print('Four')
+elif n==5 :
+	print('Five')
+elif n==6 :
+	print('Six')
+elif n==7 :
+	print('Seven')
+elif n==8 :
+	print('Eight')
+elif n==9 :
+	print('Nine')
+else :
+	print('\nPlz enter a no. from 0 to 9 ')
