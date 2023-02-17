2a0181430670f5c322809a361ffb6f3cc85ee338
kapil-malviya <etc.kapil@gmail.com>
2023-02-17 11:49:48 +0530

    another star pattern

---
+++ star-pattern-2.py
@@ -0,0 +1,24 @@
+## another star pattern
+
+
+# n=int(input('Enter number of rows : '))
+
+# for i in range(1,n+1) :
+# 	for j in range(n) :
+# 		print('* ',end='')
+# 	print()
+
+
+####     orrrrrrrrr
+
+n=int(input("Enter number of rows : "))
+for i in range(n) :
+	print('* '*n)
+
+
+
+###  output =>> n = 4  ==>>
+#    * * * *
+#    * * * *
+#    * * * *
+#    * * * *
