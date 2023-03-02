a6b5efcc30b6e24aa26fa1c8ed61c04c244ae305
kapil-malviya <etc.kapil@gmail.com>
2023-03-02 12:11:28 +0530

    how to pass

---
+++ transfer-pass-statement.py
@@ -0,0 +1,23 @@
+## transfer statements =>>
+
+#  break,  continue,  pass
+
+
+#  pass
+#      =>> it's an empty statement it won't do anything
+
+
+# def f1():
+# 	print('Hellooo.., 2nd function mein pass dala hai..')
+
+# def f2():
+# 	pass
+
+# f1()
+# f2()
+
+
+for i in range(100):
+	if i%10==0:
+		print(i)
+	else: pass
