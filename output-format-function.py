810463f59904e4d4b0036f84e953c4121a2179cb
kapil-malviya <etc.kapil@gmail.com>
2023-03-06 21:52:14 +0530

    output function : format variations

---
+++ output-format-function.py
@@ -0,0 +1,9 @@
+a = 'anil'
+b = 10000
+print('my name is : {} and my salar is {}: '.format(a,b)) 
+print('my name is : {} and my salar is {}: '.format(a,b)) 
+
+
+
+print(f"my name is : {a} and my salary is : {b}")
+print(f"my name is : {'kapil'} and my salary is : {b}")
