a846302787a502739295eea475c849a62ec0ab2c
kapil-malviya <etc.kapil@gmail.com>
2023-03-13 12:53:01 +0530

    string formating concepts

---
+++ string-formating.py
@@ -0,0 +1,10 @@
+### formating string concept :
+
+name = "Kapil"
+age = 88
+salary = 88000
+
+print("{}'s age is {} and his salary is {}".format(name,age,salary))
+print("{0}'s age is {1} and his salary is {2}".format(name,age,salary))
+print("{x}'s age is {s} and his salary is {z}".format(z=salary,s=age,x=name))
+
