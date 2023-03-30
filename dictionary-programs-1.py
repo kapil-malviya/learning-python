8d4231ead325b165c10fa774517b121397b7a132
kapil-malviya <etc.kapil@gmail.com>
2023-03-30 14:31:48 +0530

    dictionary basic programs

---
+++ dictionary-programs-1.py
@@ -0,0 +1,24 @@
+## program to take dictionary from the keyboard and print the sum of values :   
+
+'''
+d = eval(input("Enter dictionary : "))
+# d.values()
+s = sum(d.values())
+print('sum =',s)
+
+
+l = [8,88,888,808]
+s = sum(l)
+print('sum = ',s)     # 1792
+
+
+l = (8,88,888,808)
+s = sum(l)
+print('sum = ',s)     # 1792
+
+
+l = {8,88,888,808}
+s = sum(l)
+print('sum = ',s)     # 1792
+'''
+
