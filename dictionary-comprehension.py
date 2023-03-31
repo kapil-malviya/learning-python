5758d945ae9370d12561d5ebd5714510e3e7c05f
kapil-malviya <etc.kapil@gmail.com>
2023-03-31 11:40:34 +0530

    program : dictionary comprehension

---
+++ dictionary-comprehension.py
@@ -0,0 +1,15 @@
+## comprehension concept is there in dictionary : 
+
+'''
+syntax : 
+
+key:value
+squares = {x:x*x for x in range(1,11)}
+
+'''
+
+squares = {x:x*x for x in range(1,11)}
+print(squares)
+
+doubles = {x:x*2 for x in range(1,11)}
+print(doubles)
