05448fb304072f869643882be604dc95e1e75f92
kapil-malviya <etc.kapil@gmail.com>
2023-04-17 23:34:16 +0530

    module file for : 05-module-dir()

---
+++ python modules/demomodule4.py
@@ -0,0 +1,19 @@
+x = 80
+y = 88
+z = 800
+def f1():
+	print('hello kapil...')
+def f2():
+	print('have some gold')
+
+print(dir())
+
+
+
+""" =>> 
+
+		['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', 
+		'__loader__', '__name__', '__package__', '__spec__',
+		 'f1', 'f2', 'x', 'y', 'z']
+
+"""
