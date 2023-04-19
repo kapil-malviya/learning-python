e862ef0e144c66bbeda126747a31305b1c277725
kapil-malviya <etc.kapil@gmail.com>
2023-04-19 13:44:44 +0530

    working with math module

---
+++ python modules/07-module-math.py
@@ -0,0 +1,23 @@
+"""
+ =>> working with math module : 
+
+defines several functions for math operations 
+
+sqrt()
+ceil()
+floor()
+sin()
+tan()
+pow()
+
+"""
+
+
+from math import *
+
+print(sqrt(64))          # =>> 8.0
+print(ceil(8.1))         # =>> 9
+print(floor(8.1))        # =>> 8
+print(fabs(-8.1))        # =>> 8.1   ignore the sign & consider only values
+print(fabs(8.1))         # =>> 8.1
+print(abs(-8))           # =>> 8
