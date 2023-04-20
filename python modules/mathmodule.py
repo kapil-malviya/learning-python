ac38599a6a3dea0f2be80d13b2239357d54d428a
kapil-malviya <etc.kapil@gmail.com>
2023-04-20 18:06:19 +0530

    mathmodule

---
+++ python modules/mathmodule.py
@@ -0,0 +1,16 @@
+
+
+x = 888
+y = 808
+z = 800
+
+def add(a,b):
+	print('the sum : ',a+b)
+def product(a,b):
+	print('the product : ',a*b)
+def sub(a,b):
+	print('the substraction : ',a-b)
+def division(a,b):
+	print('the division : ',a/b)
+def square(a):
+	print('square of ',a,'is ',a*a)
