d2d373104c8e35be9d584dedb5ecefaf8d06b6d7
kapil-malviya <etc.kapil@gmail.com>
2023-04-27 17:51:28 +0530

    packages concept

---
+++ python packages/01-package.py
@@ -0,0 +1,21 @@
+"""
+Packages concept : 
+
+	it's an encapsulation mechanism to group related modules into a single unit,
+	package is nothing but folder or directory which represents collection of Python modules..
+
+=>>
+	this folder must contain special __init__.py python file, 
+	this file can be empty.
+	=>>	from python 3.3 verson onwards it's not mandatory to keep this file in package.
+
+=>>
+	a package can contains sub packages also.
+
+=>> advantages of package statement are : 
+	we can resolve naming conflicts
+	we can identify our components uniquely
+	it improves modularity of the application
+
+
+"""
