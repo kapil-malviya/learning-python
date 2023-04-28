dd746e5640d5121151de7aabddd1653df9e1da9b
kapil-malviya <etc.kapil@gmail.com>
2023-04-28 10:20:35 +0530

    concepts of library

---
+++ python packages/library.py
@@ -0,0 +1,25 @@
+"""
+library =>> 
+			library contains several packages :
+			(pack1, pack2, pack3...)
+
+	package =>>
+				each package contain several modules :
+				(pack1 -> module1, module2, module3)
+				(pack2 -> module1, module2, module3)
+				(pack3 -> module1, module2, module3)
+				..
+				...
+				....
+		module =>>
+				   each module contain several functions :
+				   (module1 -> x(), xx(), xxx(), xxxx())
+				   (module2 -> y(), yy(), yyy(), yyyy())
+				   (module3 -> z(), zz(), zzz(), zzzz())
+				   (module4 -> x(), xy(), xyz(), xxyy())
+				   ..
+				   ...
+				   ....
+				   
+
+"""
