9971fee282e9e9e549390c5a996eb1f01b701661
kapil-malviya <122611483+kapil-malviya@users.noreply.github.com>
2023-08-11 11:44:06 +0530

    sum of greatest two numbers

--- sum-of-two-no.py
+++ sum-of-two-no.py
@@ -1,11 +1,15 @@
 a=int(input("enter value a"))
 b=int(input("enter value b"))
 c=int(input("enter value c"))
+
 x=a+b+c
 smallest=a
+
 if a>b:
 	smallest=b
 if b>c:
 	smallest=c
+
 z=x-smallest
+
 print("z=:",z)
